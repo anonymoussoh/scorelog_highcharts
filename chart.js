0491ec6e69d1d391051b3cbf1be4f18da778bcdd
anonymoussoh <soh1991@gmail.com>
2017-01-15 03:32:41 +0900

    Adjust indent and remove more for loop

--- chart.js
+++ chart.js
@@ -1,45 +1,46 @@
 
-var global_playerlist;
-var global_turn;
 var title_name;
+var chart1; // globally available
+
+
 function change(ev){
     $('li').removeClass('selected');
 
-	var files=ev.target.files;
+    var files=ev.target.files;
 
-	var file=files[0];
-	if(!file)return;
+    var file=files[0];
+    if(!file)return;
     title_name = file.name;
 
     //Defile playerlist object to store actual data
-    var playerlist = {};
+    var playerdata = {};
     var turn = [];
 
     //Read file
-	var reader=new FileReader();
-	reader.onload=function(e){
+    var reader=new FileReader();
+    reader.onload=function(e){
       //Make array by splitting file
-      var result = reader.result.split('\n');
-      var maxline = result.length;
-      var taglist = [];
-      //Map each row
-      for(var currentnumber = 0; currentnumber < maxline ; currentnumber++){
-       //Remove \r by trim
-       var data_strip = result[currentnumber].trim().split(' ');
+    var result = reader.result.split('\n');
+    var maxline = result.length;
+    var taglist = [];
+        //Map each row
+        var lineprocessor = function(value,index){
+        //Remove \r by trim
+        var data_strip = value.trim().split(' ');
 
         //Add player info
         if(data_strip[0] === 'addplayer'){
-        playerlist[data_strip[2]] = {'name' : data_strip[3]};
+            playerdata[data_strip[2]] = {'name' : data_strip[3]};
         }
 
         //Add tag info
         if(data_strip[0] === 'tag'){
-        taglist.push(data_strip[2]);
+            taglist.push(data_strip[2]);
         }
 
         //Add turn info
         if(data_strip[0] === 'turn'){
-        turn.push(data_strip[1]);
+            turn.push(data_strip[1]);
         }
 
         //Add actual data
@@ -47,79 +48,71 @@ function change(ev){
             var current_tag = taglist[data_strip[2]];
             //Is it already added ?
             //If not, define object
-            if(!playerlist[data_strip[3]][current_tag]){
-                playerlist[data_strip[3]][current_tag] = [];
-                playerlist[data_strip[3]][current_tag] = turn.map(function(){return 0;});
-                playerlist[data_strip[3]][current_tag].pop();
+            if(!playerdata[data_strip[3]][current_tag]){
+                playerdata[data_strip[3]][current_tag] = [];
+                playerdata[data_strip[3]][current_tag] = turn.map(function(){return 0;});
+                playerdata[data_strip[3]][current_tag].pop();
             }
-            playerlist[data_strip[3]][current_tag].push(parseInt(data_strip[4],10));
+            playerdata[data_strip[3]][current_tag].push(parseInt(data_strip[4],10));
         }
+        };
+    result.forEach(lineprocessor);
+    };
+    reader.readAsText(file);
 
-      }
+    $('li').click(function() {
+        $('li').removeClass('selected');
+        $(this).addClass('selected');
+        if(title_name === undefined){
+            return;
+        }
+        var taglist = ["pop", "bnp", "mfg", "cities", "techs", "munits", "settlers", "wonders", "techout", "landarea", "settledarea", "pollution", "literacy", "spaceship", "gold", "taxrate", "scirate", "luxrate", "riots", "happypop", "contentpop", "unhappypop", "specialists", "gov", "corruption", "score", "unitsbuilt", "unitskilled", "unitslost"];
 
-   };
- global_playerlist = playerlist;
- global_turn = turn;
-	reader.readAsText(file);
-}
-
-//Above is data process
-//Below is chart rendering process
-var chart1; // globally available
-
-$('li').click(function() {
-     $('li').removeClass('selected');
-     $(this).addClass('selected');
-      if(title_name === undefined){
-      return;
-      }
-     var playerdata = global_playerlist;
-     var taglist = ["pop", "bnp", "mfg", "cities", "techs", "munits", "settlers", "wonders", "techout", "landarea", "settledarea", "pollution", "literacy", "spaceship", "gold", "taxrate", "scirate", "luxrate", "riots", "happypop", "contentpop", "unhappypop", "specialists", "gov", "corruption", "score", "unitsbuilt", "unitskilled", "unitslost"];
-     var turn = global_turn;
-
-     var dataseries = [];
-     var target = taglist[$('li').index(this)];
-       for(var item in playerdata){
-       var temp = {};
-       temp.name = playerdata[item].name;
-       temp.data = playerdata[item][target];
-       dataseries.push(temp);
-       }
-      chart1 = new Highcharts.Chart({
-         chart: {
-            height : 20 * $('li').length + 1,
-            borderColor:'gray',
-            borderWidth:1,
-            borderRadius:0,
-            renderTo: 'graph',
-            type: 'line',
-            zoomType:'xy',
-            backgroundColor:'#FFFFFF'
-         },
-         title: {
-            text: title_name + ' : ' + $(this).text()
-         },
-         plotOptions:{
-          line:{
-           lineWidth: 2,
-           marker: {
-                    enabled : false
-                    }
-          }
-         },
-         xAxis: {
-            tickInterval : 20,
-            categories: turn
-         },
-         yAxis:
-         {
-            title: {
-               text: 'Point'
+        var dataseries = [];
+        var target = taglist[$('li').index(this)];
+        for(var item in playerdata){
+            var temp = {};
+            temp.name = playerdata[item].name;
+            temp.data = playerdata[item][target];
+            dataseries.push(temp);
+        }
+        chart1 = new Highcharts.Chart({
+            chart: {
+                height : 20 * $('li').length + 1,
+                borderColor:'gray',
+                borderWidth:1,
+                borderRadius:0,
+                renderTo: 'graph',
+                type: 'line',
+                zoomType:'xy',
+                backgroundColor:'#FFFFFF'
             },
-            min:0,
-            allowDecimals:false
-         },
-         series: dataseries
-         }
-       );
-   });
+            title: {
+                text: title_name + ' : ' + $(this).text()
+            },
+            plotOptions:{
+                line:{
+                    lineWidth: 2,
+                    marker: {
+                        enabled : false
+                    }
+                }
+            },
+            xAxis: {
+                tickInterval : 20,
+                categories: turn
+            },
+            yAxis:
+            {
+                title: {
+                    text: 'Point'
+                },
+                min:0,
+                allowDecimals:false
+            },
+            series: dataseries
+            }
+           );
+       });
+
+}
